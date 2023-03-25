3a1133c2192eb68bfdef75580fde2d34adb1d313
Jackson-Vieira <jacksonvieiira12@gmail.com>
2023-03-25 02:49:38 -0300

    feat: initializate client websocket on window load

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,3 +1,8 @@
 import "./style.css";
 
 
+import { WebsocketMessage } from "./@types/websocket.type";
+import Websocket from "./websocket";
+
+const WS_URL = "ws://localhost:8080/ws";
+const websocket = new Websocket(WS_URL);
